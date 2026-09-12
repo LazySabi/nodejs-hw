3320e5cc38f23c59d54974a20fa56d614b344eca
LazySabi <KetKraft.2016@gmail.com>
2026-09-12 16:50:35 +0300

    2nd task edit

--- src/db/connectMongoDB.js
+++ src/db/connectMongoDB.js
@@ -3,10 +3,9 @@ import mongoose from 'mongoose';
 export const connectMongoDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
-
     console.log('✅ MongoDB connection established successfully');
   } catch (error) {
     console.error('❌ MongoDB connection error:', error.message);
-    throw error;
+    process.exit(1);
   }
 };
