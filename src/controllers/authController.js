cfeb90eafd862d94783f5146ecbe1f1db8a4551c
LazySabi <KetKraft.2016@gmail.com>
2026-09-13 18:41:15 +0300

    5th task edit

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -149,6 +149,7 @@ export const requestResetEmail = async (req, res) => {
 
   try {
     await sendEmail({
+      from: process.env.SMTP_FROM,
       to: user.email,
       subject: 'Password Reset',
       html,
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -19,7 +19,7 @@ export const updateUserAvatar = async (req, res) => {
       avatar: result.secure_url,
     },
     {
-      new: true,
+      returnDocument: 'after',
     },
   );
 
